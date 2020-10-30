6bd867729baa87cb311a0d993e29bb3ddd8432ca
Flash9600 <trefilovdimarus@gmail.com>
2020-10-30 15:21:48 +0400

    feat:

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,69 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor(bool){
-    this.flag = bool
-    this.alf = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    this.result=[];
+  constructor(reverse) {
+    this.alpha=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    if (reverse == false) this.reverse = false
+    else this.reverse = true
   }
-  encrypt(massage, key) {
-    if (this.massage==undefined|| this.key == undefined) throw new CustomError('THROWN');
-    this.masLit=[];
-    this.keyLit=[];
-    this.massage=this.massage.toUpperCase();
-    this.key=this.key.toUpperCase();
-    for(this.i=0; i<Math.max(massage.length, key.length); i++){
-      if(i>massage.length) {
-        this.masLit.push(massage[i%massage.length].charCode(0));
-      }else{
-        this.masLit.push(massage[i].charCode(0));
-      }
-      if(this.masLit[i]<65||this.masLit[i]>91){
-        this.keyLit.push(0)
-      }
-      if(i>key.length) {
-        this.keyLit.push(key[i%key.length].charCode(0));
-      }else{
-        this.keyLit.push(key[i].charCode(0));
-      }
 
-    };
-    for(this.i=0; i<Math.max(massage.length, key.length); i++){
-      this.result.push(String.fromCharCode(this.masLit[i]+this.keyLit[i]))
-    }
-    if (this.flag==true){
-       return this.result.join('');
-    }else return this.result.reverse().join('');
-  }
-  decrypt(massage, key) {
-    if (this.massage==undefined|| this.key == undefined) throw new CustomError('THROWN');
-    this.masLit=[];
-    this.keyLit=[];
-    this.massage=this.massage.toUpperCase();
-    this.key=this.key.toUpperCase();
-    for(this.i=0; i<Math.max(massage.length, key.length); i++){
-      if(i>massage.length) {
-        this.masLit.push(massage[i%massage.length].charCode(0));
-      }else{
-        this.masLit.push(massage[i].charCode(0));
-      }
-      if(this.masLit[i]<65||this.masLit[i]>91){
-        this.keyLit.push(0)
-      }
-      if(i>key.length) {
-        this.keyLit.push(key[i%key.length].charCode(0));
-      }else{
-        this.keyLit.push(key[i].charCode(0));
-      }
 
-    };
-    for(this.i=0; i<Math.max(massage.length, key.length); i++){
-      this.result.push(String.fromCharCode(this.masLit[i]-this.keyLit[i]))
+encrypt(message, key) {
+  let result=[]
+  if (!message || !key){throw new Error('Error')}
+  message=message.toUpperCase()
+  key=key.toUpperCase()
+  let x=0
+  for (let i=0; i<message.length;i++) {
+    if(this.alpha.indexOf(message[i])==-1){
+      result.push(message[i])
+      x++
     }
-    if (this.flag==true){
-       return this.result.join('');
-    }else return this.result.reverse().join('');
+    else result.push(this.alpha[(this.alpha.indexOf(message[i]) + this.alpha.indexOf(key[(i-x)%key.length])) % this.alpha.length])
   }
+  if (!this.reverse) {
+    return result.reverse().join('')
+  }
+  else return result.join('');
+}
+
+
+decrypt(message, key) {
+  let result=[]
+  if (!message || !key){throw new Error('Error')}
+  message=message.toUpperCase()
+  key=key.toUpperCase()
+  let x=0
+  for (let i=0; i<message.length;i++) {
+    if(this.alpha.indexOf(message[i])==-1){
+      result.push(message[i])
+      x++
+    }
+    else {
+      result.push(this.alpha[(this.alpha.indexOf(message[i]) - this.alpha.indexOf(key[(i-x)%key.length]) + this.alpha.length) % this.alpha.length])
+    }
+  }
+  if (!this.reverse) {
+    return result.reverse().join('')
+  }
+  else return result.join('');
+
+}
 }
 
 module.exports = VigenereCipheringMachine;
